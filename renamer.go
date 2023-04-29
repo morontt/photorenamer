99336cb0718177f4600c4acebeaba43b3c2de2c9
Alexander Harchenko <morontt@gmail.com>
2023-04-29 10:11:46 +0300

    second commit

--- renamer.go
+++ renamer.go
@@ -4,14 +4,13 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
 
 	"github.com/rwcarlsen/goexif/exif"
 	"github.com/rwcarlsen/goexif/mknote"
 )
 
-func main() {
-	fname := "IMG_0004.JPEG"
-
+func decodeExif(fname string) {
 	f, err := os.Open(fname)
 	if err != nil {
 		log.Fatal(err)
@@ -20,10 +19,34 @@ func main() {
 	exif.RegisterParsers(mknote.All...)
 
 	x, err := exif.Decode(f)
+	f.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	tm, _ := x.DateTime()
+	tm, err := x.DateTime()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println("Taken: ", tm)
+}
+
+func main() {
+	re := regexp.MustCompile(`(?i).jpe?g$`)
+
+	files, err := os.ReadDir(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		if !file.IsDir() {
+			fname := file.Name()
+			if re.MatchString(fname) {
+				fmt.Println(fname)
+				decodeExif(fname)
+			}
+		}
+	}
 }
